a82ffbf4d7c675b5bb64a3b45d5882f598befb29
Kevin Alexander <hashiramalop@gmail.com>
2021-07-20 13:24:32 -0600

    Nueva rama con caracteristicas como agregar notas

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+tareas = []
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", tareas=tareas)
 
 @app.route('/album')
 def album():
@@ -13,6 +14,15 @@ def album():
 @app.route('/formulario')
 def formulario():
     return render_template("form.html")
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "GET":
+        return render_template("add.html")
+    else:
+        tarea = request.form.get("tarea")
+        tareas.append(tarea)
+        return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
