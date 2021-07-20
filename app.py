ed05aa97adc395ffc99ea0f566754268fa328d93
Kevin Alexander <hashiramalop@gmail.com>
2021-07-19 20:47:41 -0600

    segundo  avance project0

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/album')
+def album():
+    return render_template("album.html")
+
+@app.route('/formulario')
+def formulario():
+    return render_template("form.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
